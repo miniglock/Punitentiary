f0b099c3a984d96a280ed74a77e4ec81a664c1fa
miniglock <davidbrin45@gmail.com>
2020-02-07 13:47:13 -0800

    Add models and homepage

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Joke, Comment, JokeFavorite, CommentFavorite
 
 # Register your models here.
+admin.site.register(Joke)
+admin.site.register(Comment)
+admin.site.register(JokeFavorite)
+admin.site.register(CommentFavorite)
