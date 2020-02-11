3b1d8d072e9628e54c0b3d2e596223d9240438b6
ernie-kuo <rongyukuo@gmail.com>
2020-02-10 21:30:52 -0800

    Comment in page editing complete

--- main_app/static/js/main.js
+++ main_app/static/js/main.js
@@ -1,13 +1,27 @@
-const update_btn = document.querySelector("#comment-update");
+const $updateBtns = $(".comment-update-btn")
+const $cancelBtns = $(".comment-update-cancel-btn")
 
-update_btn.addEventListener("click", e => {
-  console.log(`#${e.target.dataset.id}`);
-  let commentEle = document.querySelector(`#${e.target.dataset.id}`);
-  let commentValue = commentEle.innerText;
-  let inputEle = document.createElement("input");
-  inputEle.type = "text";
-  inputEle.name = "text";
+const HIDDEN_CLASS="hidden"
+const DATA_FORM="form"
+const DATA_ID="id"
 
-  let tag = ``;
-  console.log(commentValue);
-});
+function commentUpdateBtnHandler() {
+  let formId = $(this).data(DATA_FORM)
+  let commentId = $(this).data(DATA_ID)
+  let $form = $(`#${formId}`)
+  let $comment = $(`#${commentId}`)
+  $form.removeClass(HIDDEN_CLASS)
+  $comment.addClass(HIDDEN_CLASS)
+}
+
+function commentUpdateCancelBtnHandler() {
+  let formId = $(this).data(DATA_FORM)
+  let commentId = $(this).data(DATA_ID)
+  $(`#${formId}`).addClass(HIDDEN_CLASS)
+  $(`#${commentId}`).removeClass(HIDDEN_CLASS)
+}
+
+$(document).ready(function() {
+  $updateBtns.on('click', commentUpdateBtnHandler)
+  $cancelBtns.on('click', commentUpdateCancelBtnHandler)
+})
